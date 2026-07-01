a2c2c0d75463ab17dd84b71f8d10d32352f69428
Konosuke Sato <konosuke.s0912@gmail.com>
2026-07-02 00:09:57 +0900

    feat(nvim): lang.terraform extra を追加
    terraform-ls + hcl/terraform treesitter + terraform_fmt を有効化

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -51,6 +51,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.terraform" }, -- terraform-ls + hcl treesitter + fmt
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.typescript.biome" },
