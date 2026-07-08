8b95284bf7001b1efbced100dfd05112acb9faae
Konosuke Sato <konosuke.s0912@gmail.com>
2026-07-09 08:35:33 +0900

    Configure rust-analyzer source path for VS Code

--- nix/home-manager/presets/rust.nix
+++ nix/home-manager/presets/rust.nix
@@ -1,7 +1,15 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   home.sessionVariables = {
     RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+  };
+
+  home.file = lib.mkIf pkgs.stdenv.isDarwin {
+    "Library/Application Support/Code/User/settings.json".text = builtins.toJSON {
+      "rust-analyzer.server.extraEnv" = {
+        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+      };
+    };
   };
 
   home.packages = with pkgs; [
