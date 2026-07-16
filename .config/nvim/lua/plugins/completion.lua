96b29282d574924c9c9769ead6497eb619dc31fd
Konosuke Sato <konosuke.s0912@gmail.com>
2026-07-16 20:49:48 +0900

    feat(nvim): 補完ドキュメントを自動表示

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -5,6 +5,13 @@ return {
     "saghen/blink.cmp",
     opts = {
       completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 0,
+          window = {
+            border = "single",
+          },
+        },
         menu = {
           draw = {
             columns = {
