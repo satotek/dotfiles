e32d43dc559b704833be7793d16ebbd6adbbceb7
Konosuke Sato <konosuke.s0912@gmail.com>
2026-07-26 22:46:17 +0900

    refactor(nix): manage Sheldon and Starship settings

--- nix/home-manager/programs/sheldon.nix
+++ nix/home-manager/programs/sheldon.nix
@@ -1,11 +1,38 @@
-{ config, pkgs, ... }:
-let
-  homeDirectory = config.home.homeDirectory;
-  dotfilesDir = "${homeDirectory}/dotfiles";
-in
+{ ... }:
 {
-  home.packages = [ pkgs.sheldon ];
+  programs.sheldon = {
+    enable = true;
+    settings = {
+      shell = "zsh";
 
-  xdg.configFile."sheldon/plugins.toml".source =
-    config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/.config/sheldon/plugins.toml";
+      templates.defer = "{{ hooks?.pre | nl }}{% for file in files %}zsh-defer source \"{{ file }}\"\n{% endfor %}{{ hooks?.post | nl }}";
+
+      plugins = {
+        zsh-defer.github = "romkatv/zsh-defer";
+
+        zsh-completions = {
+          github = "zsh-users/zsh-completions";
+          use = [ "zsh-completions.plugin.zsh" ];
+        };
+
+        zsh-autosuggestions = {
+          github = "zsh-users/zsh-autosuggestions";
+          use = [ "zsh-autosuggestions.zsh" ];
+          apply = [ "defer" ];
+          hooks.pre = "ZSH_AUTOSUGGEST_USE_ASYNC=1";
+        };
+
+        zeno = {
+          github = "yuki-yano/zeno.zsh";
+          branch = "feature/sqlite-history-subsystem";
+        };
+
+        fast-syntax-highlighting = {
+          github = "zdharma-continuum/fast-syntax-highlighting";
+          use = [ "fast-syntax-highlighting.plugin.zsh" ];
+          apply = [ "defer" ];
+        };
+      };
+    };
+  };
 }
